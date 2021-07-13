489062ce35688c3c2ca6b3c2d9ff16c26f457777
Donatas <donkuprys@gmail.com>
2021-07-13 20:39:07 +0300

    carCount

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const CarRental = require('./CarRental');
 
 const cityBird = new CarRental('CityBird');
 
-// cityBird.intro();
+cityBird.intro();
 // Hi, we are CityBird and we have 0 cars available.
 
 cityBird.addCar('Toyota Yaris', 'black', 25);
@@ -11,25 +11,25 @@ cityBird.addCar('Tesla 3', 'white', 40);
 
 // console.log(cityBird);
 
-// cityBird.intro();
+cityBird.intro();
 // Hi, we are CityBird and we have 3 cars available.
 
 // cityBird.carPark();
-console.log(cityBird);
+// console.log(cityBird);
 // 1. Toyota Yaris (black) is for 25EUR/day.
 // 2. Volvo S40 (red) is for 20EUR/day.
 // 3. Tesla 3 (white) is for 40EUR/day.
 
 // cityBird.updateCarPrice(0, 22);
 
-cityBird.carPark();
+// cityBird.carPark();
 // // 1. Toyota Yaris (black) is for 22EUR/day.
 // // 2. Volvo S40 (red) is for 20EUR/day.
 // // 3. Tesla 3 (white) is for 40EUR/day.
 
 // cityBird.carAccident(1);
 
-cityBird.intro();
+// cityBird.intro();
 // // Hi, we are CityBird and we have 2 cars available.
 
 // cityBird.carPark();
@@ -37,7 +37,7 @@ cityBird.intro();
 // // 2. Tesla 3 (white) is for 40EUR/day.
 
 
-// cityBird.carRepair(1);
+// cityBird.carRepair(2);
 
 // cityBird.intro();
 // // Hi, we are CityBird and we have 3 cars available.
@@ -49,7 +49,7 @@ cityBird.intro();
 
 // cityBird.removeCar(0);
 
-cityBird.updateCarPrice(1, 35);
+// cityBird.updateCarPrice(1, 35);
 
 // cityBird.intro();
 // // Hi, we are CityBird and we have 2 cars available.
