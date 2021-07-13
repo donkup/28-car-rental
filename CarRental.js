5e90de3e7dece15bbae9f133d003c17476e174ac
Donatas <donkuprys@gmail.com>
2021-07-13 18:47:10 +0300

    remove car

--- CarRental.js
+++ CarRental.js
@@ -1,5 +1,63 @@
 class CarRental {
     constructor() {
+        this.list = [];
+    }
+    intro() {
+        let carCount = 0;
+        console.log('');
+        for (let i = 0; i < this.list.length; i++) {
+            const car = this.list[i];
+            if (car.working) {
+                ++carCount;
+            }
+
+        }
+        console.log(`Hi, we are CityBird and we have ${carCount} cars available `);
+        console.log('')
+    }
+
+    addCar(name, color, price) {
+        this.list.push({ name, color, price, working: true });
+    }
+
+    carPark() {
+        console.log('CARS FOR RENT');
+        for (let i = 0; i < this.list.length; i++) {
+            const car = this.list[i];
+            if (car.working) {
+
+                console.log(`${i + 1}. ${car.name} (${car.color}) is for ${car.price}EUR/day`)
+            }
+        }
+    };
+
+    updateCarPrice(index, newPrice) {
+        this.list[index].price = newPrice;
+    };
+
+    carAccident(index) {
+
+        this.list[index].working = false;
+
+    };
+
+    carRepair(index) {
+
+        this.list[index].working = true;
+    };
+
+    removeCar(index) {
+
+        const updatedCarList = []
+
+        for (let i = 0; i < this.list.length; i++) {
+            const car = this.list[i];
+            if (index !== i) {
+                updatedCarList.push(car)
+            }
+
+        }
+        this.list = updatedCarList;
     }
 }
 
--- index.js
+++ index.js
@@ -2,54 +2,58 @@ const CarRental = require('./CarRental');
 
 const cityBird = new CarRental('CityBird');
 
-cityBird.intro();
+// cityBird.intro();
 // Hi, we are CityBird and we have 0 cars available.
 
 cityBird.addCar('Toyota Yaris', 'black', 25);
 cityBird.addCar('Volvo S40', 'red', 20);
 cityBird.addCar('Tesla 3', 'white', 40);
 
-cityBird.intro();
+// console.log(cityBird);
+
+// cityBird.intro();
 // Hi, we are CityBird and we have 3 cars available.
 
-cityBird.carPark();
+// cityBird.carPark();
+console.log(cityBird);
 // 1. Toyota Yaris (black) is for 25EUR/day.
 // 2. Volvo S40 (red) is for 20EUR/day.
 // 3. Tesla 3 (white) is for 40EUR/day.
 
-cityBird.updateCarPrice(0, 22);
+// cityBird.updateCarPrice(0, 22);
 
 cityBird.carPark();
-// 1. Toyota Yaris (black) is for 22EUR/day.
-// 2. Volvo S40 (red) is for 20EUR/day.
-// 3. Tesla 3 (white) is for 40EUR/day.
+// // 1. Toyota Yaris (black) is for 22EUR/day.
+// // 2. Volvo S40 (red) is for 20EUR/day.
+// // 3. Tesla 3 (white) is for 40EUR/day.
 
-cityBird.carAccident(1);
+// cityBird.carAccident(1);
 
 cityBird.intro();
-// Hi, we are CityBird and we have 2 cars available.
+// // Hi, we are CityBird and we have 2 cars available.
 
-cityBird.carPark();
-// 1. Toyota Yaris (black) is for 22EUR/day.
-// 2. Tesla 3 (white) is for 40EUR/day.
+// cityBird.carPark();
+// // 1. Toyota Yaris (black) is for 22EUR/day.
+// // 2. Tesla 3 (white) is for 40EUR/day.
 
-cityBird.carRepair(1);
 
-cityBird.intro();
-// Hi, we are CityBird and we have 3 cars available.
+// cityBird.carRepair(1);
 
-cityBird.carPark();
-// 1. Toyota Yaris (black) is for 22EUR/day.
-// 2. Volvo S40 (red) is for 20EUR/day.
-// 3. Tesla 3 (white) is for 40EUR/day.
+// cityBird.intro();
+// // Hi, we are CityBird and we have 3 cars available.
 
-cityBird.removeCar(0);
+// cityBird.carPark();
+// // 1. Toyota Yaris (black) is for 22EUR/day.
+// // 2. Volvo S40 (red) is for 20EUR/day.
+// // 3. Tesla 3 (white) is for 40EUR/day.
+
+// cityBird.removeCar(0);
 
 cityBird.updateCarPrice(1, 35);
 
-cityBird.intro();
-// Hi, we are CityBird and we have 2 cars available.
+// cityBird.intro();
+// // Hi, we are CityBird and we have 2 cars available.
 
-cityBird.carPark();
-// 1. Volvo S40 (red) is for 20EUR/day.
-// 2. Tesla 3 (white) is for 35EUR/day.
+// cityBird.carPark();
+// // 1. Volvo S40 (red) is for 20EUR/day.
+// // 2. Tesla 3 (white) is for 35EUR/day.
